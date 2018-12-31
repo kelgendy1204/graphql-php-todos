b01ca5893efd2f3bb975730ccbb4d030329495d9
Khaled Saad <elgendy1204@gmail.com>
2018-12-31 14:33:55 +0200

    Finish user query

--- resolvers.php
+++ resolvers.php
@@ -1,6 +1,43 @@
 <?php
 
+$fakeDatabase = include __DIR__.'/fake-database.php';
+
 return [
-    'Query'    => null,
-    'Mutation' => null
+    'Query' => array(
+        'users' => function($parent, $args) use ($fakeDatabase) {
+            $search = $args['search'];
+            $users = $fakeDatabase['users'];
+
+            if(isset($search) && !empty($search)) {
+                $users = array_filter($fakeDatabase['users'], function($user) use ($search) {
+                    $name = $user['name'];
+                    if(strpos($name, $search) === false) {
+                        return false;
+                    }
+                    return true;
+                });
+            }
+
+            return array_map(function ($user) {
+                return array(
+                    'id' => $user['id'],
+                    'name' => $user['name'],
+                    'tasks' => $user['tasks']
+                );
+            }, $users);
+        }
+    ),
+    'Mutation' => null,
+    'User' => array(
+        'tasks' => function($parent) use ($fakeDatabase) {
+            $taskIds = array_values($parent['tasks']);
+            return array_map(function($taskId) use ($fakeDatabase) {
+                return $fakeDatabase['tasks'][$taskId];
+            }, $taskIds);
+        }
+    )
 ];
+
+// users(search: String): [User]
+// tasks(folderName: String, userName: String): [Task]
+// folders: [Folder]
