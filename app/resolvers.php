d674b59092c0ef52716e78b6c7c7baa5dd6ad86e
Khaled Saad <elgendy1204@gmail.com>
2019-01-02 11:54:23 +0200

    Finish resolvers

--- app/resolvers.php
+++ app/resolvers.php
@@ -30,6 +30,26 @@ return [
             $userId = $args['userId'];
             $tasks = $fakeDatabase['tasks'];
 
+            if($folderId) {
+                $tasks = array_filter($tasks, function($task) use ($folderId) {
+                    $taskFolderId = $task['folderId'];
+                    if($taskFolderId === $folderId) {
+                        return true;
+                    }
+                    return false;
+                });
+            }
+
+            if($userId) {
+                $tasks = array_filter($tasks, function($task) use ($userId) {
+                    $taskUserId = $task['userId'];
+                    if($taskUserId === $userId) {
+                        return true;
+                    }
+                    return false;
+                });
+            }
+
             return array_map(function ($task) {
                 return array(
                     'id' => $task['id'],
@@ -51,7 +71,76 @@ return [
         },
     ),
 
-    'Mutation' => null,
+    'Mutation' => array(
+        'createTask' => function($parent, $args) use ($fakeDatabase) {
+            $folderId = $args['folderId'];
+            $userId = $args['userId'];
+            $text = $args['text'];
+
+            $tasks = $fakeDatabase['tasks'];
+
+            $tasksIds = array_map(function($task) {
+                return $task['id'];
+            }, $tasks);
+
+            $newId = max($tasksIds) + 1;
+
+            $newTask = array(
+                'id'       => $newId,
+                'text'     => $text,
+                'folder' => $folderId,
+                'user'   => $userId,
+            );
+
+            $fakeDatabase['tasks'][] = $newTask;
+
+            FakeDatabase::setDatabase($fakeDatabase);
+
+            return $newTask;
+        },
+        'createUser' => function($parent, $args) use ($fakeDatabase) {
+            $name = $args['name'];
+
+            $users = $fakeDatabase['users'];
+            $usersIds = array_map(function($user) {
+                return $user['id'];
+            }, $users);
+
+            $newId = max($usersIds) + 1;
+
+            $newUser = array(
+                'id'   => $newId,
+                'name' => $name
+            );
+
+            $fakeDatabase['users'][] = $newUser;
+
+            FakeDatabase::setDatabase($fakeDatabase);
+
+            return $newUser;
+        },
+        'createFolder' => function($parent, $args) use ($fakeDatabase) {
+            $name = $args['name'];
+
+            $folders = $fakeDatabase['folders'];
+            $foldersIds = array_map(function($folder) {
+                return $folder['id'];
+            }, $folders);
+
+            $newId = max($foldersIds) + 1;
+
+            $newFolder = array(
+                'id'   => $newId,
+                'name' => $name
+            );
+
+            $fakeDatabase['folders'][] = $newFolder;
+
+            FakeDatabase::setDatabase($fakeDatabase);
+
+            return $newFolder;
+        }
+    ),
 
     'User' => array(
         'tasks' => function($parent) use ($fakeDatabase) {
